718d6bf95bdb638d96439a9624d06f52add1dbee
Patrick J Louis <lumenfuzz@gmail.com>
2018-04-17 18:13:38 +0000

    automatically backed up by learn

--- lib/create.sql
+++ lib/create.sql
@@ -10,12 +10,12 @@ CREATE TABLE projects (
 CREATE TABLE users (
   id INTEGER PRIMARY KEY,
   name TEXT,
-  age INTEGER,
+  age INTEGER
 );
 
 CREATE TABLE pledges (
   id INTEGER PRIMARY KEY,
   amount INTEGER,
   user_id TEXT,
-  project_id TEXT,
+  project_id TEXT
 );
